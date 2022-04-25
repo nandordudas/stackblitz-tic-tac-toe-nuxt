7b18cc32e6302702a89fd4e70dcc174e45ba9340
Nándor Dudás <nandor.dudas@gmail.com>
2022-04-25 15:50:48 +0000

    fix: false positive checkWinner function

--- utils/game.ts
+++ utils/game.ts
@@ -1,17 +1,6 @@
 import { hasArrayMatchValues, transpose } from './array'
 
 /**
- * Use map-list to store non-duplicated items and check if the subject is already in the map.
- *
- * @param list - The list to manage.
- * @param item - The item to add to the list.
- * @param subject - The subject to check.
- */
-const isMapResponsibleForSubject = <T>(list: Map<T, T>, item: T, subject: T) => {
-  return list.set(item, item).size === 1 && list.get(item) === subject
-}
-
-/**
  * Check if the given grid has a winner.
  *
  * @param grid - The grid to check.
@@ -19,8 +8,7 @@ const isMapResponsibleForSubject = <T>(list: Map<T, T>, item: T, subject: T) => 
  * @param length - The length of match to check.
  */
 export const checkWinner = <T, P extends number>(grid: T[][], subject: T, length: P) => {
-  // We check the length horizontally and vertically, so we don't need to check it diagonally.
-  const [_left, _right]: Map<T, T>[] = [new Map(), new Map()]
+  const [_left, _right]: T[][] = [[], []]
 
   // The grid has square dimensions, so we can use the same index for both dimensions.
   return grid.some((row, columnIndex) => {
@@ -37,15 +25,23 @@ export const checkWinner = <T, P extends number>(grid: T[][], subject: T, length
     _item = row.at(columnIndex)
 
     // Check the winner diagonally from top-left to bottom-right.
-    if (isMapResponsibleForSubject(_left, _item, subject))
-      return true
+    if (_item === subject) {
+      _left.push(_item)
+
+      if (hasArrayMatchValues(_left, subject, length))
+        return true
+    }
 
     // Get the opposite item of the current row.
     _item = row.at(length - (columnIndex + 1))
 
     // Check the winner diagonally from top-right to bottom-left.
-    if (isMapResponsibleForSubject(_right, _item, subject))
-      return true
+    if (_item === subject) {
+      _right.push(_item)
+
+      if (hasArrayMatchValues(_right, subject, length))
+        return true
+    }
 
     return false
   })
